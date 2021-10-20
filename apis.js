4ef335dde602d37632c40176d38662efa2efa73f
LauchaQ2 <89219479+LauchaQ2@users.noreply.github.com>
2021-10-20 15:29:55 -0300

    Update apis.js

--- apis.js
+++ apis.js
@@ -1,12 +1,8 @@
 const url = "https://api.jikan.moe/v3/manga/1/moreinfo";
 
 fetch(url)
-.then(response => response.json() )
+.then(response => response.JSON() )
 .then(data => {
-    let lista = document.getElementById('listaHTML');
-    lista.innerHTML = '<p>${data}</p>';
-    
     console.log(data);
-    
 })
 .catch(err=>console.log(err))
