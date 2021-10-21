342868f8249b0f10004b9510330019421e942101
LauchaQ2 <89219479+LauchaQ2@users.noreply.github.com>
2021-10-21 15:46:27 -0300

    Update mangadatabase.js

--- mangadatabase.js
+++ mangadatabase.js
@@ -1,26 +1,31 @@
-var Mangas = function (titulo, portada, mangaka){
+var Mangas = function (titulo, portada, mangaka, precio){
     this.titulo = titulo;
     this.portada = portada;
     this.mangaka = mangaka;
+    this.precio = precio;
 }
 
 // "Base de Datos"
 
-var naruto1 = new Mangas("Naruto #1", "https://m.media-amazon.com/images/I/61blY+DiS4L.jpg", "Kishimoto");
-var naruto2 = new Mangas("Naruto #2", "https://i.ibb.co/GJ6mq6t/naruto2.jpg", "Kishimoto");
-var naruto3 = new Mangas("Naruto #3", "https://i.ibb.co/VtRV5Jx/naruto3.jpg", "Kishimoto");
-var onePiece1 = new Mangas("One Piece #1", "https://m.media-amazon.com/images/I/51T6jmOtLYL.jpg", "Oda");
-var onePiece2 = new Mangas("One Piece #2", "https://i.ibb.co/RYky4jB/onepiece2.png", "Oda"); 
-var onePiece3 = new Mangas("One Piece #3", "https://i.ibb.co/Nsn2WnT/onepiece3.png", "Oda"); 
-var SNK1 = new Mangas("Attack on Titan #1", "https://images-na.ssl-images-amazon.com/images/I/51QWSFImgvL._SX342_SY445_QL70_ML2_.jpg", "Isayama");
-var SNK2 = new Mangas("Attack on Titan #2", "https://i.ibb.co/RzxtSr5/snk2.jpg", "Isayama"); 
-var SNK3 = new Mangas("Attack on Titan #2", "https://i.ibb.co/rffvF3M/snk3.jpg", "Isayama"); 
-var tokyo1 = new Mangas("Tokyo Revengers #1", "https://i.ibb.co/mSs9pLy/tokyo1.jpg", "Wakui");
-var tokyo2 = new Mangas("Tokyo Revengers #2", "https://i.ibb.co/4YJwJyC/tokyo2.jpg", "Wakui");
-var tokyo3 = new Mangas("Tokyo Revengers #3", "https://i.ibb.co/18FG16v/tokyo3.jpg.jpg", "Wakui");
-var kimetsu1 = new Mangas("Demon Slayer #1", "https://i.ibb.co/Bg1dqBH/kimetsu1.jpg", "Gotouge");
-var kimetsu2 = new Mangas("Demon Slayer #2", "https://i.ibb.co/vjqTB7H/kimetsu2.jpg", "Gotouge");
-var kimetsu3 = new Mangas("Demon Slayer #3", "https://i.ibb.co/CszwfJD/kimetsu3.jpg", "Gotouge");
+var naruto1 = new Mangas("Naruto #1", "https://m.media-amazon.com/images/I/61blY+DiS4L.jpg", "Kishimoto", 2500);
+var naruto2 = new Mangas("Naruto #2", "https://i.ibb.co/GJ6mq6t/naruto2.jpg", "Kishimoto", 2500);
+var naruto3 = new Mangas("Naruto #3", "https://i.ibb.co/VtRV5Jx/naruto3.jpg", "Kishimoto", 2500);
+var onePiece1 = new Mangas("One Piece #1", "https://m.media-amazon.com/images/I/51T6jmOtLYL.jpg", "Oda", 2500);
+var onePiece2 = new Mangas("One Piece #2", "https://i.ibb.co/RYky4jB/onepiece2.png", "Oda", 2500); 
+var onePiece3 = new Mangas("One Piece #3", "https://i.ibb.co/Nsn2WnT/onepiece3.png", "Oda", 2500); 
+var SNK1 = new Mangas("Attack on Titan #1", "https://images-na.ssl-images-amazon.com/images/I/51QWSFImgvL._SX342_SY445_QL70_ML2_.jpg", "Isayama", 2500);
+var SNK2 = new Mangas("Attack on Titan #2", "https://i.ibb.co/RzxtSr5/snk2.jpg", "Isayama", 2500); 
+var SNK3 = new Mangas("Attack on Titan #2", "https://i.ibb.co/rffvF3M/snk3.jpg", "Isayama", 2500); 
+var tokyo1 = new Mangas("Tokyo Revengers #1", "https://i.ibb.co/mSs9pLy/tokyo1.jpg", "Wakui", 2500);
+var tokyo2 = new Mangas("Tokyo Revengers #2", "https://i.ibb.co/4YJwJyC/tokyo2.jpg", "Wakui", 2500);
+var tokyo3 = new Mangas("Tokyo Revengers #3", "https://i.ibb.co/18FG16v/tokyo3.jpg.jpg", "Wakui", 2500);
+var kimetsu1 = new Mangas("Demon Slayer #1", "https://i.ibb.co/Bg1dqBH/kimetsu1.jpg", "Gotouge", 2500);
+var kimetsu2 = new Mangas("Demon Slayer #2", "https://i.ibb.co/vjqTB7H/kimetsu2.jpg", "Gotouge", 2500);
+var kimetsu3 = new Mangas("Demon Slayer #3", "https://i.ibb.co/CszwfJD/kimetsu3.jpg", "Gotouge", 2500);
+
+var funko1 = new Mangas("Naruto","https://i.ibb.co/1b4rtQW/Screenshot-2021-10-21-MANGA-KV2.png","Funko Pop", 2500);
+var funko2 = new Mangas("Luffy","https://i.ibb.co/BgSsp6j/onepiecefunkopop.jpg","Funko Pop", 3500);
+var funko3 = new Mangas("Eren (Titan Form)","https://i.ibb.co/Bq6mMcD/snkfunkopop.jpg", "Funko Pop", 2999);
 
 
     //Productos Destacados Home
@@ -45,24 +50,19 @@ var kimetsu3 = new Mangas("Demon Slayer #3", "https://i.ibb.co/CszwfJD/kimetsu3.
     document.getElementById("titlecard5").innerHTML = kimetsu1.titulo;
     document.getElementById("mangakacard5").innerHTML = "Mangaka: " + kimetsu1.mangaka;
     
+    document.getElementById("imgcard6").src = funko1.portada;
+    document.getElementById("titlecard6").innerHTML = funko1.titulo;
+    document.getElementById("mangakacard6").innerHTML = funko1.mangaka + "<br/>" + "$" + funko1.precio;
     
-    // Listado de Productos Shop
+    document.getElementById("imgcard7").src = funko2.portada;
+    document.getElementById("titlecard7").innerHTML = funko2.titulo;
+    document.getElementById("mangakacard7").innerHTML = funko2.mangaka + "<br/>" + "$" + funko2.precio;
     
-    document.getElementById("prodimg1").src = naruto2.portada;
-    document.getElementById("prodtitle1").innerHTML = naruto2.titulo;
-    document.getElementById("prodmangaka1").innerHTML = "Mangaka: " + naruto2.mangaka;
-
-   /* const naruto = [naruto1, naruto2, naruto3];
-    const onepiece = [onePiece1, onePiece2, onePiece3];
-    const attack = [SNK1, SNK2, SNK3];
-    const tokrev = [tokyo1, tokyo2, tokyo3];
-    const demon = [kimetsu1, kimetsu2, kimetsu3];*/
-
+    document.getElementById("imgcard8").src = funko3.portada;
+    document.getElementById("titlecard8").innerHTML = funko3.titulo;
+    document.getElementById("mangakacard8").innerHTML = funko3.mangaka + "<br/>" + "$" + funko3.precio;
     
 
-
-    
-    
     
     
     
