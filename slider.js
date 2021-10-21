2cb41be6962557066140d19bc72e6501b415c8b5
LauchaQ2 <89219479+LauchaQ2@users.noreply.github.com>
2021-10-21 14:16:50 -0300

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,28 @@
+var imagenes = [];
+    
+    imagenes[0] = "https://i.ibb.co/3hQjwWj/ONE-PIECE.jpg";
+    imagenes[1] = "https://i.ibb.co/b6NyVM1/Shingeki-no-Kyojin-3-x-1000.jpg";
+    imagenes[2] = "https://i.ibb.co/S5DK692/Tokyo-Revengers.jpg";
+    
+    var indiceimagenes = 0;
+
+    document.getElementById("sliderImg").src = imagenes[indiceimagenes];
+
+    function next(){
+        if (indiceimagenes < 2){
+            indiceimagenes++;
+        } else{
+            indiceimagenes = 0;
+        }
+        document.getElementById("sliderImg").src = imagenes[indiceimagenes];
+    }
+    
+    function back(){
+        if (indiceimagenes > 0){
+            indiceimagenes--;
+        } else{
+            indiceimagenes = 2;
+        }
+        document.getElementById("sliderImg").src = imagenes[indiceimagenes];
+    }
+    setInterval(next, 3000);
